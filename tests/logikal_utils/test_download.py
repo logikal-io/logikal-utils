07b110b2b6c677592a9dbb8d92f9e1b85f498c9a
GergelyKalmar <gergely.kalmar@logikal.io>
2025-11-16 13:31:33 +0100

    Update dependencies

--- tests/logikal_utils/test_download.py
+++ tests/logikal_utils/test_download.py
@@ -4,5 +4,5 @@ from logikal_utils.download import download
 
 
 def test_download(tmp_path: Path) -> None:
-    logikal_index = download('https://logikal.io', tmp_path / 'index.html')
+    logikal_index = download('https://github.com/logikal-io', tmp_path / 'index.html')
     assert 'Creating perfect digital experiences' in logikal_index.read_text()
