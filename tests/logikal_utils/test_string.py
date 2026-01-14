015c05583a6cf9ee2d8cf52fa577d01fbcdfc99d
CristinaNeacsu <cristina.neacsu@logikal.io>
2026-01-14 11:09:11 +0100

    Clean up

--- tests/logikal_utils/test_string.py
+++ tests/logikal_utils/test_string.py
@@ -9,6 +9,22 @@ def test_compact() -> None:
     assert isinstance(compact('hello world'), str)
 
 
+def test_non_string() -> None:
+    with pytest.raises(AttributeError):
+        compact(123)  # type: ignore
+    with pytest.raises(AttributeError):
+        compact([])  # type: ignore
+    with pytest.raises(AttributeError):
+        compact({'key': 'value'})  # type: ignore
+    with pytest.raises(AttributeError):
+        compact(True)  # type: ignore
+
+
+def test_basic() -> None:
+    assert compact('hello world') == 'hello world'
+    assert compact('hello') == 'hello'
+
+
 def test_whitespace() -> None:
     assert compact('hello   world') == 'hello world'
     assert compact('   hello world   ') == 'hello world'
@@ -26,11 +42,17 @@ def test_newlines() -> None:
 
 def test_newlines_whitespace() -> None:
     assert compact('hello\n   \n   world') == 'hello world'
+    assert compact('hello' + ' \n' * 99 + 'world') == 'hello world'
 
 
-def test_basic() -> None:
-    assert compact('hello world') == 'hello world'
-    assert compact('hello') == 'hello'
+def test_tabs() -> None:
+    assert compact('hello\tworld') == 'hello\tworld'
+
+
+def test_long_string() -> None:
+    text = 'hello ' * 10000
+    assert compact(text).count(' ') == 9999
+    assert len(compact(text)) == 5 * 10000 + 9999
 
 
 def test_empty() -> None:
@@ -41,31 +63,10 @@ def test_empty() -> None:
     assert not compact('\t')
 
 
-def test_tabs() -> None:
-    assert compact('hello\tworld') == 'hello\tworld'
-
-
-def test_compact_twice() -> None:
+def test_call_twice() -> None:
     text = """longer text   with
     multiple  whitespaces
     """
     compacted = compact(text)
     assert compact(text) == "longer text with multiple whitespaces"
     assert compact(compacted) == compacted
-
-
-def test_non_string() -> None:
-    with pytest.raises(AttributeError):
-        compact(123)  # type: ignore
-    with pytest.raises(AttributeError):
-        compact([])  # type: ignore
-    with pytest.raises(AttributeError):
-        compact({'key': 'value'})  # type: ignore
-    with pytest.raises(AttributeError):
-        compact(True)  # type: ignore
-
-
-def test_long_string() -> None:
-    text = 'hello ' * 10000
-    assert compact(text).count(' ') == 9999
-    assert len(compact(text)) == 5 * 10000 + 9999
