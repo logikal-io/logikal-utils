b76c501f85eb6f47dd32ffc8b0a6e629c50a006e
GergelyKalmar <gergely.kalmar@logikal.io>
2026-01-26 12:25:27 +0100

    Fix test

--- tests/logikal_utils/test_operators.py
+++ tests/logikal_utils/test_operators.py
@@ -2,6 +2,6 @@ from logikal_utils.operators import unique
 
 
 def test_unique() -> None:
-    assert list(unique([])) == []
+    assert not list(unique([]))
     assert list(unique([3, 3, 2, 1])) == [3, 2, 1]
     assert list(unique(['a', 1, 'a', 3, None])) == ['a', 1, 3, None]
