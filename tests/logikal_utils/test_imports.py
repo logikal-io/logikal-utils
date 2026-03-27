5e4ea6ad3dfbc5e9a332e97637b3f5c5146919d6
GergelyKalmar <gergely.kalmar@logikal.io>
2026-03-27 11:15:53 +0100

    Add installed

--- tests/logikal_utils/test_imports.py
+++ tests/logikal_utils/test_imports.py
@@ -1,9 +1,16 @@
 import os
 
-from logikal_utils.imports import try_import
+from logikal_utils.imports import installed, try_import
 
 
 def test_import() -> None:
     assert try_import('os') is os
     assert try_import('nonexistent_package') is None
     assert try_import('os.nonexistent_module') is None
+
+
+def test_installed() -> None:
+    assert not installed('not_installed_package')
+    assert installed('pytest_logikal')
+    assert not installed('pytest_logikal.nonexistent_module')
+    assert installed('pytest_logikal.core')
